b72d771db74aae039a21a5643979a0d7109f42ac
Nadim Kobeissi <nadim@symbolic.software>
2025-05-07 09:30:55 +0200

    Website: automatically set last updated date using GitHub API!

---
+++ website/js/updated.js
@@ -0,0 +1,26 @@
+const updatedInit = () => {
+	fetch(
+		"https://api.github.com/repos/nadimkobeissi/appliedcryptography/commits",
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			if (Array.isArray(data) && data.length > 0) {
+				const latestCommitDate = new Date(data[0].commit.author.date);
+				const options = {
+					year: "numeric",
+					month: "long",
+					day: "numeric",
+					hour: "numeric",
+					minute: "numeric",
+					hour12: true,
+				};
+				const formattedDate = latestCommitDate.toLocaleString(
+					undefined,
+					options,
+				);
+				document.getElementById("lastUpdated").innerText =
+					formattedDate + ".";
+			}
+		})
+		.catch((error) => console.error("Error fetching commit data:", error));
+};
