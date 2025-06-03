606833bd3697321e1533f3c99932fcbe027782d9
Nadim Kobeissi <nadim@symbolic.software>
2025-06-03 11:05:43 +0300

    Website: expand course schedule by default

--- website/res/js/collapsible.js
+++ website/res/js/collapsible.js
@@ -1,16 +1,8 @@
 const collapsibleInit = () => {
-	const collapsibleHeaders = document.querySelectorAll(".collapsible-header");
-	collapsibleHeaders.forEach((header) => {
+	document.querySelectorAll(".collapsible-header").forEach((header) => {
 		header.addEventListener("click", () => {
 			header.classList.toggle("active");
-			const content = header.nextElementSibling;
-			if (header.classList.contains("active")) {
-				content.style.maxHeight = `${content.scrollHeight + 14}px`;
-				content.style.height = `${content.scrollHeight + 14}px`;
-			} else {
-				content.style.maxHeight = `0px`;
-				content.style.height = `0px`;
-			}
+			header.nextElementSibling.classList.toggle("active");
 		});
 	});
 };
