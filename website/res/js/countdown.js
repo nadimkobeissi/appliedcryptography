eb82d924cefde4b82c669b2e1b5b3930ea44ae43
Nadim Kobeissi <nadim@symbolic.software>
2025-07-19 21:30:59 +0200

    Website: formatting

--- website/res/js/countdown.js
+++ website/res/js/countdown.js
@@ -15,7 +15,7 @@ const countdownInit = () => {
 				(difference % (1000 * 60 * 60)) / (1000 * 60),
 			);
 			const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-			countdownElement.textContent = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+			countdownElement.textContent = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds.`;
 		} else {
 			const container = document.getElementById("countdownContainer");
 			if (container) {
