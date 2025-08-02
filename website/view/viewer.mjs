17ada23c6c830be5c4dcf4ff66871e99483e0a34
Nadim Kobeissi <nadim@symbolic.software>
2025-08-02 21:25:37 +0200

    PDF.js fix

--- website/view/viewer.mjs
+++ website/view/viewer.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6df4a90039e82e86599669b68a5169c7a3ba274760d4077f09d3007b59694c13
-size 516053
+oid sha256:efb8df0a653aac6f639e2c0eb11e522b3ecd49b93f276a90efdc89c56e519449
+size 516025
