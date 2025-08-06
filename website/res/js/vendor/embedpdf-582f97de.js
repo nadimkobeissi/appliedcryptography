dd61fd892157611ab2fbaf9cc3d50adcc58d93ae
Nadim Kobeissi <nadim@symbolic.software>
2025-08-06 20:43:32 +0200

    Website: Migrate to EmbedPDF

--- website/res/js/vendor/embedpdf-582f97de.js
+++ website/res/js/vendor/embedpdf-582f97de.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1c17ef252393fabbae738044c67e5e3e204bed159bcc9404094bc3b4096cfd9
-size 435911
+oid sha256:8dbf4c7d69f6f429c124fa5513e4090565751e4943070ce4a70d5abf57e9baef
+size 620251
--- website/res/js/vendor/embedpdf.js
+++ website/res/js/vendor/embedpdf.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7753a357451023ec772b4e95a4b383057d22ab312a620cdc564bc2650ba51a0f
-size 50
+oid sha256:dfd73440fbdd7b6383ae910873db76916460ed24873194f714159025ff26b52e
+size 55
