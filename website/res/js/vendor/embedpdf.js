d1b2cced69548796c656b1c4f6d5fa852bbe4cf1
Nadim Kobeissi <nadim@symbolic.software>
2025-08-13 05:51:44 +0200

    Migrate repo to SHA1

--- website/res/js/vendor/embedpdf.js
+++ website/res/js/vendor/embedpdf.js
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:96c96ff50e416f23e80ba9bae8fbc9dc4ea669f19f105a4dd7917921e3b6fd09
-size 54
+export { F as default } from "./embedpdf-678b7803.js"
