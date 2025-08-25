e2484462e6f96c4e934f41fd6dfbdac7bb3589d3
Nadim Kobeissi <nadim@symbolic.software>
2025-08-25 15:04:39 +0300

    Website: class starts tomorrow, remove countdown

--- website/res/js/main.js
+++ website/res/js/main.js
@@ -1,11 +1,9 @@
 import { menuInit } from "./menu.js"
 import { collapsibleInit } from "./collapsible.js"
 import { updatedInit } from "./updated.js"
-import { countdownInit } from "./countdown.js"
 
 document.addEventListener("DOMContentLoaded", () => {
 	menuInit()
 	collapsibleInit()
 	updatedInit()
-	countdownInit()
 })
